b31c73db6706a331a0120a74070ceff70acd93de
Mihir <mihirsingh@berkeley.edu>
2025-01-14 17:32:20 -0800

    writing display update logic

--- calculator-js.js
+++ calculator-js.js
@@ -1,3 +1,4 @@
+//define arithmetic functions
 const addFunction = function(num1, num2) {
     return num1 + num2;
 }
@@ -14,6 +15,7 @@ const divideFunction = function(num1, num2) {
     return num1/num2;
 }
 
+//define function that performs operation on two numbers
 const operate = function(operator, num1, num2) {
     if (operator == '+') {
         return addFunction(num1, num2);
@@ -33,3 +35,16 @@ let firstNumber = null;
 let secondNumber = null;
 let operator = null;
 
+
+/*UPDATE DISPLAY*/
+
+//obtain references to the button and display elements
+const buttons = document.querySelectorAll('.button'); //references every div with "button" class
+const display = document.querySelector('.display-entry'); //references display <p> element with "display-entry" class
+
+//apply a function to each of the calculators' buttons
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+
+    });
+});
